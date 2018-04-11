144cfc2afa09aa7353833e24635b459c32a70159
th3mon <p.szelenberger@gmail.com>
2018-04-11 23:35:04 +0200

    When light force or none play Rickroll.

--- zadanie/js/all.js
+++ zadanie/js/all.js
@@ -4,15 +4,27 @@ $(() => {
   const $button = $form.find('button[type="submit"]');
   const audio = new Audio('imperial_march.mp3');
 
-  function isDarkSideIncluded() {
+  function isForceIncluded(force) {
     const $checkedCheckboxes = $form.find('input:checked');
 
     return $checkedCheckboxes
-      .filter((index, checkbox) => checkbox.value === 'dark_side')
+      .filter((index, checkbox) => checkbox.value === force)
       .length;
   }
 
+  function isDarkSideIncluded() {
+    return isForceIncluded('dark_side');
+  }
+
+  function isLightSideIncluded() {
+    return isForceIncluded('light_side');
+  }
+
   $form.on('submit', () => {
+    if (isLightSideIncluded()) {
+      return true;
+    }
+
     if (isDarkSideIncluded()) {
       audio.play();
 
